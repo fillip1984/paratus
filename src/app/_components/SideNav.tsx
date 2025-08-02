c445c594b8a09845d7c88d92b0e554f819c80949
fillip1984 <fillip1984@gmail.com>
2025-08-02 09:19:57 -0400

    pages stubbed out, copied over most utility components

---
+++ src/app/_components/SideNav.tsx
@@ -0,0 +1,171 @@
+"use client";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+
+import { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import {
+	FaCalendarDay,
+	FaCalendarWeek,
+	FaInbox,
+	FaPlus,
+	FaX,
+} from "react-icons/fa6";
+import Modal from "./ui/modal";
+import Link from "next/link";
+import { api } from "~/trpc/react";
+
+export default function SideNav() {
+	// const trpc = api.useUtils();
+	// const { data: inbox } = useQuery(trpc.collection.inbox.queryOptions());
+	// const { data: today } = useQuery(trpc.task.today.queryOptions());
+
+	const navItems = [
+		{
+			to: "/search",
+			label: "Search",
+			icon: FaSearch,
+		},
+		{
+			to: "/inbox",
+			label: "Inbox",
+			icon: FaInbox,
+			count: (
+				<span className="ml-auto text-xs text-gray-300">
+					{/* {inbox?.taskCount && inbox.taskCount > 0 ? inbox.taskCount : ""} */}
+				</span>
+			),
+		},
+		{
+			to: "/today",
+			label: "Today",
+			icon: FaCalendarDay,
+			count: (
+				<span className="ml-auto text-xs text-gray-300">
+					{/* {today?.length && today.length > 0 ? today.length : ""} */}
+				</span>
+			),
+		},
+		{ to: "/upcoming", label: "Upcoming", icon: FaCalendarWeek },
+	];
+	return (
+		<>
+			<nav className="bg-foreground m-2 flex min-w-[250px] flex-col gap-1 rounded-xl py-2 transition duration-150">
+				{navItems.map((item) => (
+					<Link
+						key={item.to}
+						href={item.to}
+						className={`[&.active]:text-primary [&.active]:bg-background hover:bg-background/60 mx-2 flex items-center rounded-xl p-2 transition duration-200 select-none`}
+					>
+						<item.icon className="mr-2" />
+						{item.label}
+						{/* {item.count} */}
+					</Link>
+				))}
+
+				<div className="flex items-center justify-between px-2">
+					<h4>Collections</h4>
+					{/* <button
+						onClick={() => setIsAddCollectionOpen(true)}
+						className="text-primary"
+					>
+						<FaPlus />
+					</button> */}
+				</div>
+				{/* <div ref={parentRef} className="flex flex-col gap-1 px-3 text-sm">
+					{draggableCollections.map((collection) => (
+						<Link
+							to="/collections/$collectionId"
+							params={{ collectionId: collection.id }}
+							key={collection.id}
+							data-label={collection.id}
+							className={`[&.active]:text-primary [&.active]:bg-background hover:bg-background/60 mx-2 flex items-center justify-between rounded-xl p-2 transition duration-200 select-none`}
+						>
+							<span># {collection.name}</span>
+							<span className="text-xs text-gray-300">
+								{collection.taskCount > 0 && collection.taskCount}
+							</span>
+						</Link>
+					))}
+				</div> */}
+			</nav>
+
+			{/* <AddCollectionModal
+				isOpen={isAddCollectionOpen}
+				close={() => setIsAddCollectionOpen(false)}
+			/> */}
+		</>
+	);
+}
+
+const AddCollectionModal = ({
+	isOpen,
+	close,
+}: {
+	isOpen: boolean;
+	close: () => void;
+}) => {
+	const [name, setName] = useState("");
+	const [isValid, setIsValid] = useState(false);
+	useEffect(() => {
+		setIsValid(name.trim() !== "");
+	}, [name]);
+
+	// const navigate = useNavigate();
+	// const trpc = useTRPC();
+	const queryClient = useQueryClient();
+	// const { mutate: createCollection } = useMutation(
+	// 	trpc.collection.create.mutationOptions({
+	// 		onSuccess: async (data) => {
+	// 			await queryClient.invalidateQueries({
+	// 				queryKey: trpc.collection.readAll.queryKey(),
+	// 			});
+	// 			close();
+	// 			setName("");
+	// 			navigate({
+	// 				to: "/collections/$collectionId",
+	// 				params: { collectionId: data.id },
+	// 			});
+	// 		},
+	// 	}),
+	// );
+	const handleCreateCollection = (e: React.FormEvent) => {
+		e.preventDefault();
+		if (name.trim() === "") {
+			return;
+		}
+		// createCollection({ name });
+	};
+
+	return (
+		<Modal isOpen={isOpen} close={close}>
+			<form
+				onSubmit={handleCreateCollection}
+				className="bg-foreground flex h-full w-full flex-col rounded-lg text-white"
+			>
+				<div className="flex items-center justify-between border-b border-b-white/30 px-4 py-1">
+					<h4>Add collection</h4>
+					<button type="button" onClick={close} className="text-primary">
+						<FaX />
+					</button>
+				</div>
+				<div className="flex flex-1 flex-col gap-2 p-2">
+					<input
+						type="text"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+						placeholder="Name..."
+						className="rounded border p-2"
+					/>
+				</div>
+				<div className="ml-auto flex gap-2 p-2">
+					<button type="button" onClick={close} className="button-secondary">
+						Cancel
+					</button>
+					<button type="submit" disabled={!isValid} className="button-primary">
+						Add
+					</button>
+				</div>
+			</form>
+		</Modal>
+	);
+};
---
+++ src/app/_components/ui/popupMenu.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useRef, useState } from 'react'
+
+// More ideas: https://www.youtube.com/watch?v=q6HevBxsPUM
+export default function PopupMenu({
+  button,
+  content,
+}: {
+  button: React.ReactNode
+  content: React.ReactNode
+}) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  // dismiss menu when user clicks outside
+  const dropdownRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target as Node)
+      ) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener('click', handleClick)
+
+    return () => {
+      document.removeEventListener('click', handleClick)
+    }
+  }, [dropdownRef])
+
+  return (
+    <div className="relative" ref={dropdownRef}>
+      <div
+        onClick={(e) => {
+          e.stopPropagation()
+          e.preventDefault()
+          setIsOpen((prev) => !prev)
+        }}
+        className="cursor-pointer">
+        {button}
+      </div>
+
+      {isOpen && (
+        <div
+          className="bg-foreground absolute left-0 z-[1000] rounded-lg"
+          onClick={(e) => {
+            e.stopPropagation()
+            e.preventDefault()
+            setIsOpen(false)
+          }}>
+          {content}
+        </div>
+      )}
+    </div>
+  )
+}
